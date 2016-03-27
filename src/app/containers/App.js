49358584d31063846fe880d7fbd5e678292d0923
Lee <al.s.apalat@gmail.com>
2016-03-27 19:51:57 +0800

    add files

---
+++ src/app/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+	render() {
+		return (
+			<div>
+				<h1>React Base V1</h1>		
+				<span className="customspan"><i className="fa fa-group"/> Hello World</span>
+			</div>
+		);
+	}
+}
+
+export default App;
